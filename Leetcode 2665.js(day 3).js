7ccf2412d65c45b6aa9a40b20879aac750b43fa9
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-06-17 00:46:03 +0530

    day 3

--- Leetcode 2665.js(day 3).js
+++ Leetcode 2665.js(day 3).js
@@ -3,5 +3,25 @@
  * @return {increment: FUnction, decrement: Function, reset: Funtion }
  */
 var createCounter = function(init) {
+    let count = init;
     
-}
+    function increment() {
+        return ++ count;
+    }
+
+    function decrement() {
+        return --count;
+    }
+
+    function reset() {
+        count = init;
+        return count;
+    }
+
+    return {
+        increment: increment,
+        decrement: decrement,
+        reset: reset
+
+    }
+};
