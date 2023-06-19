f6f9b50c1db1e0faf7ff46b972e272822a9c4fff
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-06-19 05:30:46 +0530

    solving through function

--- Leetcode 2635(day 4).js
+++ Leetcode 2635(day 4).js
@@ -4,5 +4,14 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
+    const res  =  [];
+
+    for (const i in arr) {
+        res.push(fn(arr[i], Number(i)));
+    }
+    return res;
+
     // return arr.map(fn);
 };
+// Functional progrMMING VS procedural programming
+// No state, no variables
